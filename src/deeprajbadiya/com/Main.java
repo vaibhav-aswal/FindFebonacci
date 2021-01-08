69ea5ffc16f344fb06eaf84b2a727fb9682ad25f
Deepraj Baidya <deeprajbaidya@gmail.com>
2021-01-08 20:40:51 +0530

    First Commit

---
+++ src/deeprajbadiya/com/Main.java
@@ -0,0 +1,9 @@
+package deeprajbadiya.com;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Hello Motherfuckers");
+	// write your code here
+    }
+}
