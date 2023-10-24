66f70ba6fb60cdb629a09ff6812e19033840eded
unknown <93672537+itsdchanda@users.noreply.github.com>
2023-10-24 23:58:58 +0530

    Optimized the code

--- src/deeprajbadiya/com/BinarySearch.java
+++ src/deeprajbadiya/com/BinarySearch.java
@@ -1,33 +1,47 @@
-import java.util.*;
-class BinarySearch
-{
-    public static void main()
-    {
-        Scanner sc = new Scanner(System.in);
-        System.out.println("Enter length of the array");
-        int l = sc.nextInt();
-        System.out.println("Enter terms");
-        int[] arr = new int[l];
-        for(int i = 0; i<l;i++)
-        {
-            arr[i] = sc.nextInt();
+import java.util.Scanner;
+
+class BinarySearch {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        // Prompt for array length
+        System.out.print("Enter length of the array: ");
+        int length = scanner.nextInt();
+
+        // Read array elements
+        int[] array = new int[length];
+        System.out.println("Enter " + length + " sorted array elements:");
+        for (int i = 0; i < length; i++) {
+            array[i] = scanner.nextInt();
         }
-        System.out.println("Enter target");
-        int target = sc.nextInt();
+
+        // Prompt for target element
+        System.out.print("Enter target: ");
+        int target = scanner.nextInt();
+
+        // Implementing Binary Search
         int start = 0;
-        int end = l-1;
-        while(start<=end)
-        {
-            int mid = start + (end-start)/2;
-            if(target<arr[mid])
-               end = mid - 1;
-            else if(target > arr[mid])
-               start = mid+1;
-            else
-            {
-               System.out.println(mid+1);
-               break;
+        int end = length - 1;
+
+        boolean isFound = false;
+
+        while (start <= end) {
+            int mid = start + (end - start) / 2;  // To avoid possible integer overflow
+
+            if (target < array[mid]) {
+                end = mid - 1;
+            } else if (target > array[mid]) {
+                start = mid + 1;
+            } else {
+                System.out.println("Target found at index: " + mid);
+                isFound = true;
+                break;
             }
+        }
+
+        if (!isFound) {
+            System.out.println("Target not found in the array.");
         }
     }
 }
